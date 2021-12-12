0013694aa509897f199e5a68954f610a9e0424a2
Zhiwotowsky <zhiwotowsky.ihor@gmail.com>
2021-12-12 20:43:04 +0200

    A file

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,36 @@
-const number0fFilms = +prompt('Склько фильмов вы уже посмотрели?', '');
+"use script";
 
-const presonalMovieDB = {
-   const: number0fFilms,
-   movies: {},
-   actors: {},
-   genres: {},
-   privat: false
- };
-
- const a = prompt('Один из последних просмотреных фильмов', ''),
-       b = prompt('На сколько оцените его?', ''),
-       c = prompt('Один из последних просмотреных фильмов', ''),
-       d = prompt('На сколько оцените его?', '');
+if (4 == 9) {
+  console.log('ok!');
+} else {
+  console.log('error');
+}
 
 
-       personalMovieDB.movies[a] = b;
-       personalMovieDB.movies[c] = d;
-       
-       console.log(personalMovieDB);
+
+/*if (num < 49) {
+  console.log('Error');
+} else if (num > 100) {
+  console.log('Много');
+} else {
+  console.log('Ok');
+}
+
+(num === 50) ? console.log('Ok') : console.log('Error'); //Тирнарый оператор*/
+
+const num = 50;
+
+switch(num) {
+  case 49:
+    console.log('Неверно');
+    break;
+  case 100:
+    console.log('Неверно');
+    break;
+  case 51:
+    console.log(' В точку');
+    break;  
+  default:
+    console.log('Не в етот раз');
+    break;    
+}
