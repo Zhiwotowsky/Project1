19f60268b7a2c6221a9e6e8ca584843341a50b88
Zhiwotowsky <zhiwotowsky.ihor@gmail.com>
2021-12-12 19:56:29 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+const number0fFilms = +prompt('Склько фильмов вы уже посмотрели?', '');
+
+const presonalMovieDB = {
+   const: number0fFilms,
+   movies: {},
+   actors: {},
+   genres: {},
+   privat: false
+ };
+
+ const a = prompt('Один из последних просмотреных фильмов', ''),
+       b = prompt('На сколько оцените его?', ''),
+       c = prompt('Один из последних просмотреных фильмов', ''),
+       d = prompt('На сколько оцените его?', '');
+
+
+       personalMovieDB.movies[a] = b;
+       personalMovieDB.movies[c] = d;
+       
+       console.log(personalMovieDB);
