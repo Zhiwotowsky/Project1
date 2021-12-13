d5d198efcfc66b4b6f509c1685e84499f9b2da76
Zhiwotowsky <zhiwotowsky.ihor@gmail.com>
2021-12-13 19:28:21 +0200

    cycle

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,24 @@
 "use script";
 
-if (4 == 9) {
-  console.log('ok!');
-} else {
-  console.log('error');
+let num = 50;
+
+/*while (num <= 55) {
+  console.log(num);
+  num++;
+}*/
+
+/*do {
+  console.log(num);
+  num++;
 }
+while (num < 55);*/
 
+for (let i = 1; i < 10; i++) { //ЦИКЛИ
+  if (i === 6) {
+      //break; // цикл іде до 5
+      continue; //цикл пропускає 6 
+  }
+  console.log(i);
 
-
-/*if (num < 49) {
-  console.log('Error');
-} else if (num > 100) {
-  console.log('Много');
-} else {
-  console.log('Ok');
-}
-
-(num === 50) ? console.log('Ok') : console.log('Error'); //Тирнарый оператор*/
-
-const num = 50;
-
-switch(num) {
-  case 49:
-    console.log('Неверно');
-    break;
-  case 100:
-    console.log('Неверно');
-    break;
-  case 51:
-    console.log(' В точку');
-    break;  
-  default:
-    console.log('Не в етот раз');
-    break;    
+ 
 }
